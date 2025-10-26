7c4f3c9eeb393a4a0cdcf278df0775131766c49d
mrpra <mrpravudatta10@gmail.com>
2025-10-26 22:16:14 +0530

    added hash

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,16 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 export const routes: Routes = [
     {
         path: '',
-        loadChildren: () =>
-            import('./features/features.module').then(m => m.FeaturesModule)
-    }
+        loadChildren: () => import('./features/features.module').then(m => m.FeaturesModule)
+    },
+    { path: '**', redirectTo: '' }
 ];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes, { useHash: true })],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/features/features-routing.module.ts
+++ src/app/features/features-routing.module.ts
@@ -3,14 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { ChatComponent } from './chat/chat.component';
 
-const routes: Routes = [
+export const routes: Routes = [
 	{ path: '', component: ChatComponent },
 	{ path: 'login', component: LoginComponent },
 	{ path: 'chat', component: ChatComponent }, 
 ];
 
 @NgModule({
-	imports: [RouterModule.forRoot(routes, { useHash: true })],
+	imports: [RouterModule.forChild(routes)],
 	exports: [RouterModule]
 })
 export class FeaturesRoutingModule {}
